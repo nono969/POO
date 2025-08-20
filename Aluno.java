3ca6eb6e8e63d648a5a41e5de3bce722b223fc62
Noemy <145844934+nono969@users.noreply.github.com>
2025-08-20 10:58:53 -0300

    Create Aluno.java

---
+++ Aluno.java
@@ -0,0 +1,49 @@
+public class Aluno{
+//atributos
+private String nome;
+private int matricula;
+private double nota1;
+private double nota2;
+
+//construtor
+public Aluno(String nome, int matricula, double nota1, double nota2){
+    this.nome = nome;
+    this.matricula = matricula;
+    this.nota1 = nota1;
+    this.nota2 = nota2;
+}
+//gets e sets
+public String getNome(){return nome;}
+public void setNome(String nome){ this.nome = nome;}
+
+public int getMatricula(){ return matricula; }
+public void setMatricula(int matricula){ this.matricula = matricula; }
+
+public double getNota1(){ return nota1; }
+public void setNota1(double nota1){ this.nota1 = nota1; }
+
+public double getNota2(){ return nota2; }
+public void setNota2(double nota2){ this.nota2 = nota2; }
+
+//metodos
+public void calcularMedia(){
+    if(nota1 + nota2) /2;
+
+    System.out.println("a media das duas notas é: ");   
+
+}
+public void situacao(){
+    if(medias > 7.0){
+    System.out.println("aprovado");  
+        } else {
+        
+    System.out.println("reprovado");
+        }
+}
+public void exibirInfo(){
+    System.out.println("aluno(a): " + nome + "\nmatricula: " + matricula +
+    "\nnotas:  " + nota1 + nota2 + "\nmedias: " + calcularMedia +
+    "\nsituacao" + situacao);
+    
+    }
+}
