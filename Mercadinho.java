ea52f727495b64ba18735f281b27c576b13746cb
Noemy <145844934+nono969@users.noreply.github.com>
2025-11-12 11:33:50 -0300

    Add files via upload

---
+++ Mercadinho.java
@@ -0,0 +1,32 @@
+public class Mercadinho{
+    private Cliente[] clientes = new Cliente[5];
+    private int qtde = 0;
+    
+
+
+//metodos
+    public void adicionarCliente(Cliente c){
+        if(qtde < this.clientes.length)
+            clientes[qtde] = c;
+            qtde++;
+        } else {
+            System.out.println("Cheio!");
+    }
+    
+    public double calcularTotal(){
+        double total = 0;
+        for(int i = 0; i < qtde; i++){
+            total += clientes[i].calcularPagamento();
+    }
+
+    public void imprimirClientes(){
+        for(int i = 0; i < qtde; i++){
+            Cliente c = clientes[i];
+        System.out.println(cliente[i]get.nome)
+        }
+    
+    
+    
+       
+    }
+}
